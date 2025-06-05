7e219906bd309eeb467039401852638c877ef66a
umamaheshwari-pavi <umapavi200012@gmail.com>
2025-06-05 08:33:50 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
 {path:'', component:AboutComponent},
 {path:'aboutlink', component:AboutComponent},
 {path:'resumelink', component:ResumeComponent},
-{path:'portfoliolink', component:PortfolioComponent}
+{path:'portfoliolink', component:PortfolioComponent},
+{path:'projectlink', component:ProjectComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MyportComponent } from './myport/myport.component';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     MyportComponent,
     AboutComponent,
     ResumeComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
